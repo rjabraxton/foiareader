c26363fc9f48db7495193f2dd1363f27ce058554
Richard Braxton <richard@braxton.one>
2020-09-16 22:23:51 -0700

    basic navbar.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,94 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import StarIcon from "@material-ui/icons/StarBorder";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
 import HomeIcon from "@material-ui/icons/Home";
 
 import ConversationPage from "./TextConversations/ConversationPage";
 import HomePage from "./HomePage/HomePage";
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {"Copyright © "}
-//       {/* <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{" "} */}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    marginBottom: "10px",
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    paddingRight: "20px",
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+    color: "white",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <Router>
-      <div>
+    <React.Fragment>
+      <Router>
         <CssBaseline />
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <HomeIcon />
-            </IconButton>
-            <Typography variant="h6">
-              <Link to="/">News</Link>
+        <AppBar
+          position="static"
+          color="primary"
+          elevation={0}
+          className={classes.appBar}
+        >
+          <Toolbar className={classes.toolbar}>
+            <Typography
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              <RouterLink color="textPrimary" to="/" className={classes.link}>
+                Portland Records Reader
+              </RouterLink>
             </Typography>
-            <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
-        <main>
-          <div />
-          <Container>
-            <Switch>
-              <Route path="/record/:id" component={ConversationPage} />
-              <Route exact path="/" component={HomePage} />
-            </Switch>
-            <Box pt={4}>{/* <Copyright /> */}</Box>
-          </Container>
-        </main>
-      </div>
-    </Router>
+        <Container>
+          <Switch>
+            <Route path="/record/:id" component={ConversationPage} />
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+          <Box pt={4}>{/* <Copyright /> */}</Box>
+        </Container>
+      </Router>
+    </React.Fragment>
   );
 }
